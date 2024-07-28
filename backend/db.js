76b51e8fd1bd4430c4dc7e199cc473f1d4d14c6d
Ayush Jaiswal <119951206+leviosa-1@users.noreply.github.com>
2024-07-28 21:17:59 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoose_URI = "mongodb://localhost:27017/enotebook";
+const mongoose_URI = "mongodb+srv://ayush:Pranjal%403105@cluster0.6amg2no.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongo = async () => {
   try {
